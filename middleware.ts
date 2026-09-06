0212cd9b61440c3a165f35a163258827821ed9e8
FannyFleurs <adm.fannyfleurs@gmail.com>
2026-09-07 00:42:49 +0200

    Update middleware.ts

--- middleware.ts
+++ middleware.ts
@@ -283,5 +283,5 @@ export function middleware(req: NextRequest) {
 }
 
   export const config = {
-  matcher: '/((?!_next/static|_next/image|favicon.ico).*)',
+matcher: '/((?!api/cloudprnt(?:/|$)|_next/static|_next/image|favicon.ico).*)',
 };
